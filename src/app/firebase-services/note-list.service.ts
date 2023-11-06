4e302c3cbe3f95223d6969532d833d018f44e785
Jasmin Raith <jasmin-raith@outlook.com>
2023-11-06 12:38:02 +0100

    database access

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  firestore: Firestore = inject(Firestore);
+
   constructor() { }
+
+  //  const itemCollection = collection(this.firestore, 'items');
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId:string, docId:string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
+
 }
